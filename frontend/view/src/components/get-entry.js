92673d49e26370706e99f14646f733877c5ddf8b
Siil777 <118188578+Siil777@users.noreply.github.com>
2024-12-02 19:48:21 +0200

    Update get-entry.js

--- frontend/view/src/components/get-entry.js
+++ frontend/view/src/components/get-entry.js
@@ -7,7 +7,7 @@ const TaskBoard = () => {
 
     const fetchTasks = async () => {
         try {
-            const response = await fetch("http://localhost:5000/get/task", {
+            const response = await fetch("https://for-server-side-2.onrender.com/get/task", {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json",
